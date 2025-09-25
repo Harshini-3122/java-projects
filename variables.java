652a73522bcbbe7e3c724d8159b35d11e5bc66be
Harshini-3122 <harshiniramesh259@gmail.com>
2025-09-25 08:17:13 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,28 @@
+public class variables {
+    public static void main(String[] args){
+        String name = "Harshini";
+        System.out.println(name+" hello");
+        String firstname = "krishna";
+        String lastname = "Priya";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+        final int number = 50;
+        System.out.println(number);
+        int value;
+        value = 40;
+        System.out.println(value);
+
+        int a = 5;
+        int b = 31;
+        int c = 12;
+        System.out.println(a+b+c);
+
+        int x=5,y=2,z=4;
+        System.out.println(x+y+z);
+
+        int e,f,g;
+        e=f=g= 10;
+        System.out.println(e+f+g);
+
+    }
+}
