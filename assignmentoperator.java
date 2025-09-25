06637695d9b78f8aac2e141210af92844bd70c7f
Harshini-3122 <harshiniramesh259@gmail.com>
2025-09-25 08:14:29 +0530

    Add files via upload

---
+++ assignmentoperator.java
@@ -0,0 +1,9 @@
+public class assignmentoperator {
+    public static void main(String[] args){
+        int x = 5;
+        x <<= 5;
+        System.out.println(x);
+        // x=x+5 (x += 5)
+
+    }
+}
