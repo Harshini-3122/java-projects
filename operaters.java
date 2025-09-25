fcae3e67c1afc66c4d529040506e3a1d48d23084
Harshini-3122 <harshiniramesh259@gmail.com>
2025-09-25 08:16:17 +0530

    Add files via upload

---
+++ operaters.java
@@ -0,0 +1,15 @@
+public class operaters {
+    public static void main(String[] args){
+        int x = 16;
+        int y = 8;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+        int a = 31;
+        a--;
+        System.out.println(a);
+    }
+}
