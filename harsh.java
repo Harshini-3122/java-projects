d364426f65f25ff1fc8c14b9b9bae2262771d929
Harshini-3122 <harshiniramesh259@gmail.com>
2025-09-25 08:15:39 +0530

    Add files via upload

---
+++ harsh.java
@@ -0,0 +1,42 @@
+public class harsh {
+    public static void main (String[] args){
+        System.out.println(5+3);
+        System.out.println("apple");
+        System.out.println(6*3);
+        System.out.println(5%6);
+        //logic /*logic
+
+        //boolean
+        boolean tuesday = true;
+        System.out.println(tuesday);
+
+        //byte
+        byte rollno = 122;
+        System.out.println(rollno);
+
+        //char
+        char section ='m';
+        System.out.println(section);
+
+        //short
+        short eaxmno = 31222;
+        System.out.println(eaxmno);
+
+        //int
+        int callnum = 1234567890;
+        System.out.println(callnum);
+
+        //long
+        long number =  12345689752213l;
+        System.out.println(number);
+
+        //float
+        float pi = 1.2346873652f;
+        System.out.println(pi);
+
+        //double
+        double deci = 2.638494085634372920235d;
+        System.out.println(deci);
+    }
+
+}
